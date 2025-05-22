ec24e41db385b4b1e8c62d95057c78f987837553
Fredrick Ogallo <fredogallo1@gmail.com>
2025-05-22 06:48:09 +0300

    Personal finance calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -14,3 +14,4 @@ projected_savings = annual_savings_without_interest + interest_earned
 #print result
 print(f"Your monthly savings are: ${monthly_savings:.2f}")
 print(f"Projected savings after one year, with interest, is: ${projected_savings:.2f}")
+
